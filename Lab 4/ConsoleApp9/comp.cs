3e10dde198a7634018ea94ff0050df5cd8833ce1
Akremn <119876836+Akremn@users.noreply.github.com>
2023-12-05 21:18:15 +0200

    Add files via upload

---
+++ Lab 4/ConsoleApp9/comp.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp9
+{
+    public class MyComplex : IMyNumber<MyComplex>
+    {
+        public double Real { get; private set; } // Дійсна частина комплексного числа
+        public double Imaginary { get; private set; } // Уявна частина комплексного числа
+
+        public MyComplex(double re, double im)
+        {
+            Real = re;
+            Imaginary = im;
+        }
+
+        public MyComplex Add(MyComplex that)
+        {
+            return new MyComplex(Real + that.Real, Imaginary + that.Imaginary); // Додавання комплексних чисел
+        }
+
+        public MyComplex Subtract(MyComplex that)
+        {
+            return new MyComplex(Real - that.Real, Imaginary - that.Imaginary); // Віднімання комплексних чисел
+        }
+
+        public MyComplex Multiply(MyComplex that)
+        {
+            return new MyComplex(Real * that.Real - Imaginary * that.Imaginary,
+                Real * that.Imaginary + Imaginary * that.Real); // Множення комплексних чисел
+        }
+
+        public MyComplex Divide(MyComplex that)
+        {
+            double denominator = that.Real * that.Real + that.Imaginary * that.Imaginary;
+
+            if (denominator == 0)
+            {
+                throw new DivideByZeroException("Ділення на нуль.");
+            }
+
+            double real = (Real * that.Real + Imaginary * that.Imaginary) / denominator;
+            double imaginary = (Imaginary * that.Real - Real * that.Imaginary) / denominator;
+
+            return new MyComplex(real, imaginary); // Ділення комплексних чисел
+        }
+
+        public override string ToString()
+        {
+            return $"{Real} + {Imaginary}i"; // Повернення комплексного числа у вигляді "Дійсна частина + Уявна частина i"
+        }
+    }
+}
---
+++ Lab 4/ConsoleApp9/frac.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Numerics;
+
+namespace ConsoleApp9
+{
+    public class MyFrac : IMyNumber<MyFrac>, IComparable<MyFrac>
+    {
+        public BigInteger Nom { get; private set; } // Чисельник
+        public BigInteger Denom { get; private set; } // Знаменник
+
+        public MyFrac(BigInteger nom, BigInteger denom)
+        {
+            if (denom == 0)
+            {
+                throw new ArgumentException("Знаменник не може дорівнювати нулю.");
+            }
+
+            Nom = nom;
+            Denom = denom;
+            Simplify(); // Спрощення дробу після ініціалізації
+        }
+
+        public MyFrac(int nom, int denom) : this(new BigInteger(nom), new BigInteger(denom)) { } // Конструктор, який приймає значення типу int і викликає конструктор для BigInteger
+
+        public MyFrac Add(MyFrac that)
+        {
+            // Додавання дробів
+            return new MyFrac(
+                BigInteger.Add(BigInteger.Multiply(Nom, that.Denom), BigInteger.Multiply(Denom, that.Nom)),
+                BigInteger.Multiply(Denom, that.Denom)
+            );
+        }
+
+        public MyFrac Subtract(MyFrac that)
+        {
+            // Віднімання дробів
+            return new MyFrac(
+                BigInteger.Subtract(BigInteger.Multiply(Nom, that.Denom), BigInteger.Multiply(Denom, that.Nom)),
+                BigInteger.Multiply(Denom, that.Denom)
+            );
+        }
+
+        public MyFrac Multiply(MyFrac that)
+        {
+            // Множення дробів
+            return new MyFrac(BigInteger.Multiply(Nom, that.Nom), BigInteger.Multiply(Denom, that.Denom));
+        }
+
+        public MyFrac Divide(MyFrac that)
+        {
+            if (that.Nom == 0)
+            {
+                throw new DivideByZeroException("Ділення на нуль.");
+            }
+
+            // Ділення дробів
+            return new MyFrac(BigInteger.Multiply(Nom, that.Denom), BigInteger.Multiply(Denom, that.Nom));
+        }
+
+        private void Simplify()
+        {
+            // Спрощення дробу до найменших цілих чисел
+            BigInteger gcd = BigInteger.GreatestCommonDivisor(Nom, Denom);
+            Nom /= gcd;
+            Denom /= gcd;
+        }
+
+        public override string ToString()
+        {
+            return $"{Nom}/{Denom}"; // Повернення дробу у форматі "Чисельник/Знаменник"
+        }
+
+        public int CompareTo(MyFrac that)
+        {
+            // Порівняння двох дробів
+            MyFrac thisNormalized = new MyFrac(Nom, Denom);
+            MyFrac thatNormalized = new MyFrac(that.Nom, that.Denom);
+
+            thisNormalized.Simplify();
+            thatNormalized.Simplify();
+
+            return (int)(thisNormalized.Nom * thatNormalized.Denom - thisNormalized.Denom * thatNormalized.Nom);
+        }
+    }
+}
