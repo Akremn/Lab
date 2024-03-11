5e6039fd8f3e14789e12fe48b30e713cce2c5c50
Akremn <119876836+Akremn@users.noreply.github.com>
2024-03-11 22:15:04 +0200

    Add files via upload

---
+++ Zav 1/ConsoleApp12/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Threading;
+
+namespace timer
+{
+    public delegate void TimerEvent();
+
+    public class Timer
+    {
+        private TimerEvent methodToExecute;
+        private int interval;
+        private bool isRunning;
+
+        public Timer(int intervalInSeconds, TimerEvent method)
+        {
+            interval = intervalInSeconds * 1000; // convert seconds to milliseconds
+            methodToExecute = method;
+            isRunning = false;
+        }
+
+        public void Start()
+        {
+            if (!isRunning)
+            {
+                isRunning = true;
+                Thread thread = new Thread(RunTimer);
+                thread.Start();
+            }
+        }
+
+        private void RunTimer()
+        {
+            while (isRunning)   
+            {
+                Thread.Sleep(interval);
+                methodToExecute();
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Example usage:
+            Timer timer = new Timer(2, SomeMethod); // Fires SomeMethod every 2 seconds
+            timer.Start();
+            Timer timer1 = new Timer(4, SomeMethod1); // Fires SomeMethod every 4 seconds
+            timer1.Start();
+
+            // Keep the console application running
+            Console.ReadLine();
+        }
+
+        static void SomeMethod()
+        {
+            Console.WriteLine("Executing SomeMethod...");
+        }
+        static void SomeMethod1()
+        {
+            Console.WriteLine("Executing SomeMethodааааа...");
+        }
+    }
+}
---
+++ Zav 2/ConsoleApp13/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Linq;
+
+namespace ArrayUtils
+{
+    public class Program
+    {
+        public delegate bool FilterDelegate(int number, int k);
+        public static void Main()
+        {
+            Console.Write("Введіть масив чисел через пробіл: ");
+            int[] originalArray = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            Console.Write("Введіть значення k: ");
+            int k = int.Parse(Console.ReadLine());
+
+            FilterDelegate filterDelegate = IsMultiple;
+
+            // Using Where method
+            int[] filteredArrayWhere = FilterArrayUsingWhere(originalArray, k, filterDelegate);
+            Console.WriteLine("Filtered Array using Where method:");
+            foreach (var num in filteredArrayWhere)
+            {
+                Console.Write(num + " ");
+            }
+            Console.WriteLine();
+
+            // Using custom implementation
+            int[] filteredArrayCustom = FilterArrayWithoutLibrary(originalArray, k, filterDelegate);
+            Console.WriteLine("Filtered Array using custom implementation:");
+            foreach (var num in filteredArrayCustom)
+            {
+                Console.Write(num + " ");
+            }
+            Console.WriteLine();
+        }
+        public static int[] FilterArrayUsingWhere(int[] inputArray, int k, FilterDelegate filter)
+        {
+            var result = inputArray.Where(x => filter(x, k)).ToArray();
+            return result;
+        }
+        public static int[] FilterArrayWithoutLibrary(int[] inputArray, int k, FilterDelegate filter)
+        {
+            int count = 0;
+            for (int i = 0; i < inputArray.Length; i++)
+            {
+                if (filter(inputArray[i], k))
+                {
+                    count++;
+                }
+            }
+
+            int[] resultArray = new int[count];
+            int index = 0;
+            for (int i = 0; i < inputArray.Length; i++)
+            {
+                if (filter(inputArray[i], k))
+                {
+                    resultArray[index] = inputArray[i];
+                    index++;
+                }
+            }
+
+            return resultArray;
+        }
+        static bool IsMultiple(int number, int k)
+        {
+            if (number == 0)
+            {
+                return false;
+            }
+            return number % k == 0;
+        }
+    }
+}
