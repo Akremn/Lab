737049553b15859cdb237c355c2cd2b4de83fc12
Akremn <119876836+Akremn@users.noreply.github.com>
2024-03-18 22:18:16 +0200

    Add files via upload

---
+++ Lab2 Calculator/WpfApp4/Page1.xaml.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+
+namespace WpfApp4
+{
+    /// <summary>
+    /// Interaction logic for Page1.xaml
+    /// </summary>
+    public partial class Page1 : Page
+    {
+        public Page1()
+        {
+            InitializeComponent();
+            foreach (UIElement el in GroupButton.Children)
+            {
+                if (el is Button)
+                {
+                    ((Button)el).Click += ButtonClick;
+                }
+            }
+        }
+        private void ButtonClick(object sender, RoutedEventArgs e)
+        {
+            string textButton = ((Button)e.OriginalSource).Content.ToString();
+            if (textButton == "C"){text.Clear();}
+            else if (textButton == "CE")
+            {
+                if (text.Text.Length > 0)
+                {
+                    text.Text = text.Text.Remove(text.Text.Length - 1);
+                }
+            }
+            else if (textButton == "⟵")
+            {
+                if (text.Text.Length > 0)
+                {
+                    text.Text = text.Text.Remove(text.Text.Length - 1);
+                }
+            }
+            else if (textButton == "="){text.Text = new DataTable().Compute(text.Text, null).ToString();}
+            else if (textButton == "Pi"){text.Text += "3.14";}
+            else if (textButton == "√")
+            {
+                double num;
+                if (Double.TryParse(text.Text, out num))
+                {
+                    if (num >= 0)
+                    {
+                        text.Text = Math.Sqrt(num).ToString();
+                    }
+                    else
+                    {
+                        return;
+                    }
+                }
+            }
+            else if (textButton == "^2")
+            {
+                double num;
+                if (Double.TryParse(text.Text, out num))
+                {
+                    text.Text = Math.Pow(num, 2).ToString();
+                }
+            }
+            else if (textButton == "log")
+            {
+                double num;
+                if (Double.TryParse(text.Text, out num))
+                {
+                    text.Text = Math.Log(num, 2).ToString();
+                }
+            }
+            else if (textButton == ">>>>"){}
+            else {text.Text += textButton;}
+        }
+        private void But1_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new Page2());
+        }
+
+        private void MainFrame_Navigated(object sender, NavigationEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Lab2 Calculator/WpfApp4/Page2.xaml.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp4
+{
+    /// <summary>
+    /// Interaction logic for Page2.xaml
+    /// </summary>
+    public partial class Page2 : Page
+    {
+        public Page2()
+        {
+            InitializeComponent();
+            foreach (UIElement el in GroupButton.Children)
+            {
+                if (el is Button)
+                {
+                    ((Button)el).Click += ButtonClick;
+                }
+            }
+        }
+        private void ButtonClick(object sender, RoutedEventArgs e)
+        {
+            string textButton = ((Button)e.OriginalSource).Content.ToString();
+            if (textButton == "C")
+            {
+                text.Clear();
+            }
+            else if (textButton == "CE")
+            {
+                if(text.Text.Length > 0)
+                {
+                    text.Text = text.Text.Remove(text.Text.Length - 1);
+                }
+                
+            }
+            else if (textButton == "⟵")
+            {
+                if (text.Text.Length > 0)
+                {
+                    text.Text = text.Text.Remove(text.Text.Length - 1);
+                }
+            }
+            else if (textButton == "=")
+            {
+                text.Text = new DataTable().Compute(text.Text, null).ToString();
+            }
+            else if (textButton == ">>>>"){}
+            else
+            {
+                text.Text += textButton;
+            }
+        }
+        private void text_TextChanged(object sender, TextChangedEventArgs e)
+        {
+                
+        }
+
+        private void But2_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new Page1());
+        }
+    }
+}
